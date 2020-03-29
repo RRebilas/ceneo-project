eca79fa0f5d378f286ff991126be4f97f1a64cc1
RRebilas <rrafal7@gmail.com>
2020-03-29 14:44:11 +0200

    Database initialization

---
+++ ceneo/schema.sql
@@ -0,0 +1,25 @@
+DROP TABLE IF EXISTS products;
+DROP TABLE IF EXISTS opinions;
+
+CREATE TABLE products (
+                          product_id INTEGER PRIMARY KEY NOT NULL UNIQUE,
+                          product_name VARCHAR (255) NOT NULL,
+                          product_url VARCHAR (255) NOT NULL
+);
+
+CREATE TABLE opinions (
+                          opinion_id INTEGER PRIMARY KEY NOT NULL UNIQUE,
+                          author TEXT NOT NULL,
+                          stars INTEGER NOT NULL,
+                          useful INTEGER NOT NULL,
+                          useless INTEGER NOT NULL,
+                          content TEXT NOT NULL,
+                          date_of_issue TEXT NOT NULL,
+                          recommendation TEXT,
+                          purchased TEXT,
+                          date_of_purchase TEXT,
+                          cons TEXT,
+                          pros TEXT,
+                          product_id int NOT NULL,
+                          FOREIGN KEY(product_id) REFERENCES products(product_id)
+)
